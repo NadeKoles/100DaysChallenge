dd66d7fefba492e4914f6e3da2da7cfab38ddf2d
NadeKoles <enot9296@gmail.com>
2026-01-14 22:43:28 +0300

    Fix registration

--- 100DaysChallenge/App/DaysChallengeApp.swift
+++ 100DaysChallenge/App/DaysChallengeApp.swift
@@ -6,16 +6,27 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import GoogleSignIn
 
 @main
 struct DaysChallengeApp: App {
     @StateObject private var appState = AppState()
+    @StateObject private var authVM = AuthViewModel()
+    
+    init() {
+        FirebaseApp.configure()
+    }
     
     var body: some Scene {
         WindowGroup {
             RootView()
                 .environmentObject(appState)
                 .environmentObject(ChallengeStore.shared)
+                .environmentObject(authVM)
+                .onOpenURL { url in
+                    GIDSignIn.sharedInstance.handle(url)
+                }
         }
     }
 }
